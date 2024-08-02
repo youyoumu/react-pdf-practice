50a25396008b7543f9760e74c73ef6a137cd47b9
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-08-02 11:08:19 +0700

    Use css for full screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,18 @@ import { PDFViewer } from '@react-pdf/renderer'
 import Invoice from './components/Invoice'
 import invoiceData from './data/invoice_data'
 import './App.css'
-import { useState, useEffect } from 'react'
 
 export default function App() {
-  const [{ width, height }, setSize] = useState({ width: 0, height: 0 })
-
-  useEffect(() => {
-    const handleResize = () => {
-      setSize({ width: window.innerWidth, height: window.innerHeight - 5 })
-    }
-    window.addEventListener('resize', handleResize)
-    handleResize()
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <PDFViewer width={width} height={height} className="app">
+      <PDFViewer
+        style={{
+          position: 'absolute',
+          border: 0,
+          height: '100%',
+          width: '100%'
+        }}
+      >
         <Invoice invoice={invoiceData} />
       </PDFViewer>
     </div>
