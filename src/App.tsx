e7c68ce026298652578a83931a38b3e3b99c625f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-08-02 11:03:15 +0700

    Add all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { PDFViewer } from '@react-pdf/renderer'
+import Invoice from './components/Invoice'
+import invoiceData from './data/invoice_data'
 import './App.css'
+import { useState, useEffect } from 'react'
 
 export default function App() {
-  return <div>test</div>
+  const [{ width, height }, setSize] = useState({ width: 0, height: 0 })
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight - 5 })
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <PDFViewer width={width} height={height} className="app">
+        <Invoice invoice={invoiceData} />
+      </PDFViewer>
+    </div>
+  )
 }
---
+++ src/data/invoice_data.ts
@@ -0,0 +1,45 @@
+const invoiceData = {
+  id: '66ac4b9f12fc22a48b902bb0',
+  invoice_no: '202005-84',
+  balance: '$3,032.48',
+  company: 'ECOSYS',
+  email: 'adasherman@ecosys.com',
+  phone: '+1 (842) 507-2272',
+  address: '112 Bainbridge Street, Calpine, Oklahoma, 4321',
+  trans_date: '2019-09-11',
+  due_date: '2019-10-30',
+  items: [
+    {
+      sno: 1,
+      desc: 'ea enim est consequat nostrud',
+      qty: 3,
+      rate: 414.91
+    },
+    {
+      sno: 2,
+      desc: 'occaecat exercitation ullamco dolore elit',
+      qty: 8,
+      rate: 321
+    },
+    {
+      sno: 3,
+      desc: 'labore nulla sit irure aliqua',
+      qty: 5,
+      rate: 939.41
+    },
+    {
+      sno: 4,
+      desc: 'magna cupidatat ea tempor aliquip',
+      qty: 8,
+      rate: 48.85
+    },
+    {
+      sno: 5,
+      desc: 'tempor aliqua deserunt laborum pariatur',
+      qty: 8,
+      rate: 413.55
+    }
+  ]
+}
+
+export default invoiceData
